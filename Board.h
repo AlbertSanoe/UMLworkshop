e37e75e16aa7b66a35f9a2a38d664dfab6b5e25d
AlbertSanoe <ymd7823@outlook.com>
2022-07-08 22:29:46 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Card_Industry_Abstract.h

--- Board.h
+++ Board.h
@@ -5,4 +5,22 @@
 #ifndef UMLWORKSHOP_BOARD_H
 #define UMLWORKSHOP_BOARD_H
 
+#include "Factory_Card.h"
+
+class Board{
+private:
+    QList<Card_RealEstate*>RealEstate_Card={};
+    QList<Card_Transport*>Transport_Card={};
+    QList<Card_Energy*>Energy_Card={};
+    QList<Card_Action*>Action_Card={};
+
+    void Set_RealEstate_Card_List();
+    void Set_Transport_Card_List();
+    void Set_Energy_Card_List();
+    void Set_Action_Card_List();
+
+public:
+    Board();
+};
+
 #endif //UMLWORKSHOP_BOARD_H
