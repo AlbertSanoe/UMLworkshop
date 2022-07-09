8a3682a1e0d6b10f0e3b5a29b1a66a17a4f74d5a
AlbertSanoe <ymd7823@outlook.com>
2022-07-09 22:47:01 +0800

    202207092246

--- Factory_Square.h
+++ Factory_Square.h
@@ -10,7 +10,7 @@
 class Factory_Square{
 public:
     Factory_Square()=default;
-    View_Square_Abstract* ReturnViewSquare(int x_pos,int y_pos,int wide,int height,int pos_board);
+    static View_Square_Abstract* ReturnViewSquare(int x_pos,int y_pos,int wide,int height,int pos_board);
 };
 
 #endif //UMLWORKSHOP_FACTORY_SQUARE_H
--- View_Square_Treasure_Chance.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Su on 2022/7/7.
-//
-
-#include "View_Square_Treasure_Chance.h"
