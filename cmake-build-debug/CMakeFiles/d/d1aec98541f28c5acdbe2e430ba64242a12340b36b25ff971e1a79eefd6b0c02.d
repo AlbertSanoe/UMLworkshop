40802f5d8853e3627ee61751c900ad080d1d9876
AlbertSanoe <ymd7823@outlook.com>
2022-07-10 15:46:47 +0800

    202207101546

--- cmake-build-debug/CMakeFiles/d/d1aec98541f28c5acdbe2e430ba64242a12340b36b25ff971e1a79eefd6b0c02.d
+++ cmake-build-debug/CMakeFiles/d/d1aec98541f28c5acdbe2e430ba64242a12340b36b25ff971e1a79eefd6b0c02.d
@@ -215,6 +215,15 @@ UMLworkshop_autogen/timestamp: \
   D:/out\ software/CLion\ 2021.3.4/bin/mingw/x86_64-w64-mingw32/include/vadefs.h \
   D:/out\ software/CLion\ 2021.3.4/bin/mingw/x86_64-w64-mingw32/include/wchar.h \
   D:/out\ software/CLion\ 2021.3.4/bin/mingw/x86_64-w64-mingw32/include/wctype.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/QFile \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/QJsonArray \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/QJsonObject \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/QQueue \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/QString \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/QStringList \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/QTextStream \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/QtGlobal \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qabstractitemmodel.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qalgorithms.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qarraydata.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qatomic.h \
@@ -222,12 +231,22 @@ UMLworkshop_autogen/timestamp: \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qbasicatomic.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qbytearray.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qbytearraylist.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qcborcommon.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qcborvalue.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qchar.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qcompilerdetection.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qconfig.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qcontainerfwd.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qcontainertools_impl.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qcontiguouscache.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qcoreapplication.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qcoreevent.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qdatastream.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qdatetime.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qdebug.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qeventloop.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qfile.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qfiledevice.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qflags.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qgenericatomic.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qglobal.h \
@@ -236,9 +255,14 @@ UMLworkshop_autogen/timestamp: \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qhashfunctions.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qiodevice.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qiterator.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qjsonarray.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qjsonobject.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qjsonvalue.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qline.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qlist.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qlocale.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qlogging.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qmap.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qmargins.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qmetatype.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qnamespace.h \
@@ -250,10 +274,13 @@ UMLworkshop_autogen/timestamp: \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qpair.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qpoint.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qprocessordetection.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qqueue.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qrect.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qrefcount.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qregexp.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qregularexpression.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qscopedpointer.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qset.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qshareddata.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qsharedpointer.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qsharedpointer_impl.h \
@@ -267,36 +294,88 @@ UMLworkshop_autogen/timestamp: \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qsysinfo.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qsystemdetection.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qtcore-config.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qtextstream.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qtypeinfo.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qurl.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/quuid.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qvariant.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qvarlengtharray.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qvector.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtCore/qversiontagging.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/QMouseEvent \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qbrush.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qcolor.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qcursor.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qevent.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qfont.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qfontinfo.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qfontmetrics.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qguiapplication.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qicon.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qimage.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qinputmethod.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qkeysequence.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qmatrix.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qpaintdevice.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qpainter.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qpainterpath.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qpalette.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qpen.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qpixelformat.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qpixmap.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qpolygon.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qregion.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qrgb.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qrgba64.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qtextoption.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qtgui-config.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qtguiglobal.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qtouchdevice.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qtransform.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qvalidator.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qvector2d.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qwindowdefs.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtGui/qwindowdefs_win.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QApplication \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QComboBox \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QDialog \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QGraphicsPixmapItem \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QGraphicsRectItem \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QGraphicsScene \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QGraphicsView \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QHBoxLayout \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QLabel \
   D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QMainWindow \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QMessageBox \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QPushButton \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/QVBoxLayout \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qabstractbutton.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qabstractitemdelegate.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qabstractscrollarea.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qabstractslider.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qabstractspinbox.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qapplication.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qboxlayout.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qcombobox.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qdialog.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qframe.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qgraphicsitem.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qgraphicsscene.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qgraphicsview.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qgridlayout.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qlabel.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qlayout.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qlayoutitem.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qmainwindow.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qmessagebox.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qpushbutton.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qrubberband.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qscrollarea.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qsizepolicy.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qslider.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qstyle.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qstyleoption.h \
+  D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qtabbar.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qtabwidget.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qtwidgets-config.h \
   D:/qt/creator/5.15.2/mingw81_64/include/QtWidgets/qtwidgetsglobal.h \
@@ -333,11 +412,65 @@ UMLworkshop_autogen/timestamp: \
   D:/qt/creator/5.15.2/mingw81_64/lib/cmake/Qt5Widgets/Qt5WidgetsConfigVersion.cmake \
   D:/qt/creator/5.15.2/mingw81_64/lib/cmake/Qt5Widgets/Qt5WidgetsMacros.cmake \
   D:/qt/creator/5.15.2/mingw81_64/lib/cmake/Qt5Widgets/Qt5Widgets_QWindowsVistaStylePlugin.cmake \
+  ../Board.cpp \
+  ../Board.h \
+  ../Busticket.cpp \
+  ../Busticket.h \
   ../CMakeLists.txt \
-  ../Factory_Abstract.h \
-  ../Factroy_Abstract.cpp \
+  ../Card_Abstract.cpp \
+  ../Card_Abstract.h \
+  ../Card_Action.cpp \
+  ../Card_Action.h \
+  ../Card_Energy.cpp \
+  ../Card_Energy.h \
+  ../Card_RealEstate.cpp \
+  ../Card_RealEstate.h \
+  ../Card_Transport.cpp \
+  ../Card_Transport.h \
+  ../Controller_Object.cpp \
+  ../Controller_Object.h \
+  ../Controller_View.cpp \
+  ../Controller_View.h \
+  ../Die.cpp \
+  ../Die.h \
+  ../Event_Abstract.cpp \
+  ../Event_Abstract.h \
+  ../Factory_Card.cpp \
+  ../Factory_Card.h \
+  ../Factory_Square.cpp \
+  ../Factory_Square.h \
   ../MainWindow.cpp \
   ../MainWindow.h \
+  ../Player.cpp \
+  ../Player.h \
+  ../Square_Industry_Abstract.cpp \
+  ../Square_Industry_Abstract.h \
+  ../View_Abstract.cpp \
+  ../View_Abstract.h \
+  ../View_Board.cpp \
+  ../View_Board.h \
+  ../View_House.cpp \
+  ../View_House.h \
+  ../View_Piece.cpp \
+  ../View_Piece.h \
+  ../View_Player.cpp \
+  ../View_Player.h \
+  ../View_Square_Abstract.cpp \
+  ../View_Square_Abstract.h \
+  ../View_Square_Idle.cpp \
+  ../View_Square_Idle.h \
+  ../View_Square_Prison.cpp \
+  ../View_Square_Prison.h \
+  ../View_Square_Start.cpp \
+  ../View_Square_Start.h \
+  ../View_Square_Tax.cpp \
+  ../View_Square_Tax.h \
+  ../View_Square_WithCard.cpp \
+  ../View_Square_WithCard.h \
+  ../View_StartingGame.cpp \
+  ../View_StartingGame.h \
+  ../View_Turn.cpp \
+  ../View_Turn.h \
   CMakeFiles/3.21.1/CMakeCCompiler.cmake \
   CMakeFiles/3.21.1/CMakeCXXCompiler.cmake \
   CMakeFiles/3.21.1/CMakeRCCompiler.cmake \
